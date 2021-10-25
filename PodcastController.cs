26ec70f07dd9e5a5a1859f9225691129b390f05b
melvindavis9 <64192637+melvindavis9@users.noreply.github.com>
2021-10-25 14:15:05 +0200

    Add files via upload

---
+++ PodcastController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace BusinessLayer
+{
+    public class PodcastController
+    {
+        private List<Podcast> listOfPodcasts;
+
+        public PodcastController()
+        {
+            listOfPodcasts = new List<Podcast>();
+            GetPodcasts = listOfPodcasts;
+            addPodcast(13, "The Matrix", "1h", "Informatik");
+            addPodcast(32, "GhostAdventures", "10h", "Paranormal");
+        }
+
+
+        public void addPodcast(int avsnitt, string namn, string uppdatering, string kategori)
+        {
+            Podcast podcast = new Podcast(avsnitt, namn, uppdatering, kategori);
+            listOfPodcasts.Add(podcast);
+
+        }
+
+        public List<Podcast> GetPodcasts { get; }
+    }
+}
