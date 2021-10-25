2609095f2d2b8d515451bf4422b518f35e3827ce
melvindavis9 <64192637+melvindavis9@users.noreply.github.com>
2021-10-25 13:49:13 +0200

    Add files via upload

---
+++ DataAccessLayer/RSS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace DataAccessLayer
+{
+    public class RSS
+    {
+        /*
+        XmlReader reader = XmlReader.Create("https://rss.aftonbladet.se/rss2/small/pages/sections/senastenytt/");
+        SyndicationFeed feed = SyndicationFeed.Load(reader);
+        Console.WriteLine("Title: " + feed.Title.Text);
+        Console.WriteLine("Description: " + feed.Description.Text);
+
+            foreach (SyndicationItem item in feed.Items)
+            {
+                Console.WriteLine(item.Title.Text);
+                Console.WriteLine("->" + item.Summary.Text);
+                Console.WriteLine();
+
+
+            }*/
+
+    }
+
+}
---
+++ Models/Podcast.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Models
+{
+    public class Podcast
+    {
+        private int avsnitt;
+        private string namn;
+        private string uppdatering;
+        private string kategori;
+
+        public Podcast(int avsnitt, string namn, string uppdatering, string kategori)
+        {
+            this.avsnitt = avsnitt;
+            this.namn = namn;
+            this.uppdatering = uppdatering;
+            this.kategori = kategori;
+        }
+
+        public int Avsnitt
+        {
+            get
+            {
+                return avsnitt;
+            }
+        }
+        public string Namn
+        {
+            get
+            {
+                return namn;
+            }
+        }
+        public string Uppdatering
+        {
+            get
+            {
+                return uppdatering;
+            }
+        }
+        public string Kategori
+        {
+            get
+            {
+                return kategori;
+            }
+        }
+    }
+}
